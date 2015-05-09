67cda42a3cf26c041ee561a83220744995937e6d
辛文義 <eric.hsin@gmail.com>
2015-05-09 21:07:17 +0800

    q

--- account/delete.php
+++ account/delete.php
@@ -1,12 +1,12 @@
 <?php
 /**
  *
- * @package    mahara
+ * @package    mahara 
  * @subpackage core
  * @author     Catalyst IT Ltd
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL version 3 or later
  * @copyright  For copyright information on Mahara, please see the README file distributed with this software.
- *
+ *  by Eric
  */
 
 define('INTERNAL', 1);
